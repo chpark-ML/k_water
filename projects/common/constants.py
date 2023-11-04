92e77f2f75ac2c3ac067f23c436da6fe42111327
chpark-ML <bch0322@gmail.com>
2023-11-04 13:01:54 +0000

    fold index updates

--- projects/common/constants.py
+++ projects/common/constants.py
@@ -6,6 +6,7 @@ from pycocotools.coco import COCO
 DATA_ROOT_PATH = Path('/data/k_water/dataset')
 DATA_ROOT_PATH_TRAIN = DATA_ROOT_PATH / 'train'
 DATA_ROOT_PATH_TEST = DATA_ROOT_PATH / 'test'
+NUM_FOLD = 10
 
 TRAIN_DATA_IMAGE = sorted(DATA_ROOT_PATH_TRAIN.rglob("train_*.png"))
 TEST_DATA_IMAGE = sorted(DATA_ROOT_PATH_TEST.rglob("test_*.png"))
@@ -15,3 +16,6 @@ ANSWER_SAMPLE = DATA_ROOT_PATH / "labels" / "answer_sample.json"
 
 coco=COCO(TRAIN_DATA_ANNOT)
 NUM_CLASS = len(coco.getCatIds())
+IMAGE_FEATURES = ['img_id', "num_annot", "average_area", "entropy"]
+for i in range(NUM_CLASS):
+    IMAGE_FEATURES.append(f"is_class_{i}")
